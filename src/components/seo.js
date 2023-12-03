9d71142f6a562180ed2596e95802401bb9196072
Saleel <saleel@saleel.xyz>
2023-12-03 12:57:04 +0530

    chore: fix title

--- src/components/seo.js
+++ src/components/seo.js
@@ -26,11 +26,11 @@ const Seo = ({ description, title, children }) => {
   )
 
   const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
+  // const defaultTitle = site.siteMetadata?.title
 
   return (
     <>
-      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+      <title>{title}</title>
       <meta name="description" content={metaDescription} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
